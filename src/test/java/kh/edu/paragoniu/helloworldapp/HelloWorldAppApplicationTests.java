7e19ea688e89003164bfb4a8afe5adba28121c4c
Kimseak Sok <seakkim6@gmail.com>
2026-02-12 10:27:19 +0700

    Initial commit

---
+++ src/test/java/kh/edu/paragoniu/helloworldapp/HelloWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.edu.paragoniu.helloworldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
