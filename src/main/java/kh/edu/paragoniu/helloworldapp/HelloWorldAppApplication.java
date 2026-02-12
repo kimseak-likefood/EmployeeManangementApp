3ad5b2c00e07d681e43f4fe0c8af56b4777b42c2
Kimseak Sok <seakkim6@gmail.com>
2026-02-12 10:33:51 +0700

    home page

--- src/main/java/kh/edu/paragoniu/helloworldapp/HelloWorldAppApplication.java
+++ src/main/java/kh/edu/paragoniu/helloworldapp/HelloWorldAppApplication.java
@@ -2,12 +2,20 @@ package kh.edu.paragoniu.helloworldapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldAppApplication.class, args);
+    }
+
+    @GetMapping
+    public String home(){
+        return "Welcome to my Web Site. </br> <b>Hello World</b> ";
     }
 
 }
