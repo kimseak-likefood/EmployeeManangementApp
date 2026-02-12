f5e3404b10bd1351d78e79ce7b40d584907514db
Kimseak Sok <seakkim6@gmail.com>
2026-02-12 11:42:41 +0700

    added index.html and thymeleaf dependency

--- src/main/java/kh/edu/paragoniu/helloworldapp/HelloWorldAppApplication.java
+++ src/main/java/kh/edu/paragoniu/helloworldapp/HelloWorldAppApplication.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class HelloWorldAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldAppApplication.class, args);
-    }
-
-    @GetMapping
-    public String home(){
-        return "Welcome to my Web Site. </br> <b>Hello World</b> ";
     }
 
 }
